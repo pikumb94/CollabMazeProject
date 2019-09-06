17c642f21cd4d65263b6e43c29d09c42b0f1724a
PikUmb <pikumb94@gmail.com>
2019-09-06 18:43:22 +0200

    More prefabs and basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
