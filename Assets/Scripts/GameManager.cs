e00a24df589da4ba58e859a6a332c67267647bd6
PikUmb <pikumb94@gmail.com>
2019-09-08 19:08:21 +0200

    added counter and raycast in deadends

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
-    public float countdownMinutes = 450.0f;
+    public float countdownSeconds = 450.0f;
     private TextMeshProUGUI tmp;
     private int minutes, seconds;
+    private bool isGameOver = false;
 
     private void Awake()
     {
@@ -21,16 +23,30 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        tmp = GameObject.Find("TimerText").GetComponent<TextMeshProUGUI>();
-        minutes = (int) countdownMinutes / 60;
-        seconds = (int) countdownMinutes % 60;
 
-        tmp.SetText(minutes.ToString()+":"+seconds.ToString());
+        tmp = GameObject.Find("TimerText").GetComponent<TextMeshProUGUI>();
+        DisplayTimeFormatted();
+        InvokeRepeating("DecreaseCounter", 1f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void DecreaseCounter (){
+        countdownSeconds--;
+        if (countdownSeconds >= 0)
+            DisplayTimeFormatted();
+        else
+            isGameOver = true;
+    }
+
+    void DisplayTimeFormatted()
+    {
+        minutes = (int)countdownSeconds / 60;
+        seconds = (int)countdownSeconds % 60;
+        tmp.SetText(minutes.ToString() + ":" + String.Format("{0:00}", seconds));
     }
 }
