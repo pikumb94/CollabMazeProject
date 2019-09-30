c8b62d0d21e7a35ff688c3e3867af3330da1185e
unknown <pikumb94@gmail.com>
2019-09-30 19:20:03 +0200

    Some basic tips addded in UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -34,33 +34,36 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-
+        if (Input.GetKeyDown("0"))
+        {
+            SceneManager.LoadScene(0);
+        }
         if (Input.GetKeyDown("1")) {
-           SceneManager.LoadScene(0);
+           SceneManager.LoadScene(1);
         }
         if (Input.GetKeyDown("2"))
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(2);
         }
         if (Input.GetKeyDown("3"))
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(3);
         }
         if (Input.GetKeyDown("4"))
         {
-            SceneManager.LoadScene(3);
+            SceneManager.LoadScene(4);
         }
         if (Input.GetKeyDown("5"))
         {
-            SceneManager.LoadScene(4);
+            SceneManager.LoadScene(5);
         }
         if (Input.GetKeyDown("6"))
         {
-            SceneManager.LoadScene(5);
+            SceneManager.LoadScene(6);
         }
         if (Input.GetKeyDown("7"))
         {
-            SceneManager.LoadScene(6);
+            SceneManager.LoadScene(7);
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
